e2a98236ed7fb8215209be145216ff457b96f622
Reine <reinebjames@gmail.com>
2024-09-08 21:36:11 +0800

    Carousel
    Hero Section - Team Photo Slideshow

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
 import Homepage from "@/pages/Homepage"
 /*import classes from "./page.module.css";*/
 /*import Image from 'next/image'*/
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main>
       <Header/>
+      <Hero/>
       <Homepage/>
       <Footer/>
 
